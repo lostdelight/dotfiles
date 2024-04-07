c5996f517e870fb4ec3cd471443289a2e697974f
İnanç <157127531+lostdelight@users.noreply.github.com>
2024-04-07 10:28:26 +0300

    Update feh.sh

--- feh.sh
+++ feh.sh
@@ -1,13 +1,19 @@
 #!/bin/bash                                                               
                                                                       
 # Get the absolute path to the directory containing the images            
-image_dir=(dir)                                               
+image_dir=/home/alp/Pictures/favs                                              
                                                                       
 # Get a list of all the images in the directory                           
 images=($(find "$image_dir" -type f -name '*.png' -o -name '*.jpg' -o -name '*.jpeg'))                                                                
                                                                       
 # Choose a random image from the list                                     
 random_image=${images[RANDOM % ${#images[@]}]}                            
+
+# Pywalfox
+pywalfox update
+
+# Pywal
+wal -i "$random_image"
+
+
                                                                       
-# Set the background image using feh                                      
-feh --bg-scale --no-fehbg "$random_image"  
